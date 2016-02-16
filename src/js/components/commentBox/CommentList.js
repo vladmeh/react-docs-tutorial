3bd7327746f01c6d520107cc4193159272de5fb4
vladmeh <vladmeh@gmail.com>
2016-02-17 02:15:59 +0300

    example 5

--- src/js/components/commentBox/CommentList.js
+++ src/js/components/commentBox/CommentList.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+import Comment from "./commentList/Comment"
+
 export default class CommentList extends React.Component  {
   render() {
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        <Comment autor="Pete Hunt">This is one comment</Comment>
+        <Comment autor="Jordan Wake">This is *another* comment</Comment>
       </div>
     );
   }
