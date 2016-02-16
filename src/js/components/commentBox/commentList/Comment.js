006f72cfe10308a7f6ff05b0c0c88259179dd239
vladmeh <vladmeh@gmail.com>
2016-02-17 02:33:41 +0300

    example 7

--- src/js/components/commentBox/commentList/Comment.js
+++ src/js/components/commentBox/commentList/Comment.js
@@ -2,13 +2,18 @@ import React from "react";
 import Marked from "marked";
 
 export default class Comment extends React.Component  {
+  rawMarkup() {
+    var rawMarkup = Marked(this.props.children.toString(), {sanitize: true});
+    return { __html: rawMarkup };
+  }
+
   render() {
     return (
       <div className="comment">
         <h2 className="commentAutor">
           {this.props.autor}
         </h2>
-        {Marked(this.props.children.toString())}
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
       </div>
     );
   }
