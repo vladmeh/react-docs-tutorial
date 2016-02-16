111df1688a451da48a511765af07ebd888cc7441
vladmeh <vladmeh@gmail.com>
2016-02-17 02:46:38 +0300

    example 8-9

--- src/js/example.js
+++ src/js/example.js
@@ -5,4 +5,9 @@ import CommentBox from "./components/CommentBox"
 
 const app = document.getElementById('app');
 
-ReactDOM.render(<CommentBox />, app);
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+ReactDOM.render(<CommentBox data={data}/>, app);
