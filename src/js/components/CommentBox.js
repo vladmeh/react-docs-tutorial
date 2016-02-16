bfdfede0edf55cb15f61c96918e1140873c2f416
vladmeh <vladmeh@gmail.com>
2016-02-17 01:41:41 +0300

    example 2

--- src/js/components/CommentBox.js
+++ src/js/components/CommentBox.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 
 export default class CommentBox extends React.Component {
   render() {
--- src/js/example.js
+++ src/js/example.js
@@ -5,14 +5,4 @@ import CommentBox from "./components/CommentBox"
 
 const app = document.getElementById('app');
 
-// var CommentBox = React.createClass({
-//   render: function() {
-//     return (
-//       <div className="commentBox">
-//         Hello, world! I am a CommentBox.
-//       </div>
-//     );
-//   }
-// });
-
 ReactDOM.render(<CommentBox />, app);
