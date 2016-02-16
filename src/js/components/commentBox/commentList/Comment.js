b1123c36f0fd67faed783e4243ab769c8ef0c2a8
vladmeh <vladmeh@gmail.com>
2016-02-17 01:52:50 +0300

    example 4

--- src/js/components/commentBox/commentList/Comment.js
+++ src/js/components/commentBox/commentList/Comment.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default class Comment extends React.Component  {
-
+  render() {
+    return (
+      <div className="comment">
+        <h2 className="commentAutor">
+          {this.props.autor}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
 }
