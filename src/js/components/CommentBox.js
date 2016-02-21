a0481a81b064aa135eeca46f7e30ed490dd94671
vladmeh <vladmeh@gmail.com>
2016-02-21 22:24:47 +0300

    Example 12. Reactive state.

--- src/js/components/CommentBox.js
+++ src/js/components/CommentBox.js
@@ -4,11 +4,15 @@ import CommentList from "./commentBox/CommentList"
 import CommentForm from "./commentBox/CommentForm"
 
 export default class CommentBox extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {data: []};
+  }
   render() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data}/>
+        <CommentList data={this.state.data}/>
         <CommentForm />
       </div>
     );
