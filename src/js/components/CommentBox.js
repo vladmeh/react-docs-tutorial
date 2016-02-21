da522a067e97ca7bbd4aea3c6c2d3874f609fd33
vladmeh <vladmeh@gmail.com>
2016-02-22 01:43:21 +0300

    Example 13. Updating state.

--- src/js/components/CommentBox.js
+++ src/js/components/CommentBox.js
@@ -1,4 +1,5 @@
 import React from "react";
+import $ from "jquery";
 
 import CommentList from "./commentBox/CommentList"
 import CommentForm from "./commentBox/CommentForm"
@@ -8,6 +9,22 @@ export default class CommentBox extends React.Component {
     super(props);
     this.state = {data: []};
   }
+
+  componentDidMount() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        console.log(data);
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  }
+
   render() {
     return (
       <div className="commentBox">
--- src/js/example.js
+++ src/js/example.js
@@ -5,9 +5,5 @@ import CommentBox from "./components/CommentBox"
 
 const app = document.getElementById('app');
 
-var data = [
-  {id: 1, author: "Pete Hunt", text: "This is one comment"},
-  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-];
 
-ReactDOM.render(<CommentBox data={data} url="data/comments"/>, app);
+ReactDOM.render(<CommentBox url="data/comments.json"/>, app);
